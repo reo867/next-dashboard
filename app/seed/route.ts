4e51d350ada6a05d34685d1ba60bdf703a176368
miwa reou <reou@miwanoMacBook-Pro.local>
2025-05-02 12:49:23 +0900

    bycryptjs

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,5 +1,4 @@
-import bcrypt from 'bcrypt';
-// import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
